48193119a7cd22c13cb85c127dc5bdcb991fb632
Eduardo-DevJs <dudev2001@gmail.com>
2024-01-09 15:05:05 -0300

    Criando pagina home

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,11 +1,32 @@
-import React from 'react'
+import { BsCartPlus } from 'react-icons/bs';
 
 const Home = () => {
   return (
     <div>
-      <h1>Página Home</h1>
-    </div>
-  )
-}
+      <main className="w-full max-w-7xl px-4 m ax-auto">
+        <h1 className="font-bold text-2xl mb-4 mt-10 text-center">
+          Produtos em alta
+        </h1>
 
-export default Home
+        <div className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-5">
+          <section className="w-full">
+            <img
+              className="rounded-lg max-h-70 mb-2"
+              src=""
+              alt="logo do produto"
+            />
+            <p className="font-medium mt-2 mb-2">Airpods Pro</p>
+            <div className="flex items-center">
+              <strong className="text-zinc-700/90">R$ 1000</strong>
+              <button className="bg-zinc-900 p-1 rounded">
+                <BsCartPlus size={20} color="#FFF" />
+              </button>
+            </div>
+          </section>
+        </div>
+      </main>
+    </div>
+  );
+};
+
+export default Home;
