627b793933070c3c8b305202b3836d14d57e103d
Eduardo-DevJs <dudev2001@gmail.com>
2024-01-09 19:47:40 -0300

    Feature/pagina carrinho de compras

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -3,7 +3,7 @@ import { BsCartPlus } from 'react-icons/bs';
 const Home = () => {
   return (
     <div>
-      <main className="w-full max-w-7xl px-4 m ax-auto">
+      <main className="w-full max-w-7xl px-4 mx-auto">
         <h1 className="font-bold text-2xl mb-4 mt-10 text-center">
           Produtos em alta
         </h1>
